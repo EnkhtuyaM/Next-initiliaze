dd122b3fa0d34d1c0850ae55c5d6e6426970a99b
Enkhtuya Myagmar <23LP6946@G401-CC0091.local>
2024-01-16 00:25:00 -0800

    from school

--- first-next-app/src/pages/index.js
+++ first-next-app/src/pages/index.js
@@ -1,9 +1,17 @@
-import {Button, TextField, Card} from '@/components'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+// import { Button, TextField, Card } from "@/components";
+// import Link from "next/link";
+// import styles from "@/styles/pages/Home.module.css";
+import { Tailwind } from "./tailwindcss";
 
 export default function Home() {
   return (
+    <div className="{styles.container}">
+      {/* <h1 className="{styles.title}">Home</h1>
+      <Link href={"/abc"}>Move To Abc</Link>
+      <Button />
+      <TextField /> */}
+      <Tailwind />
+      {/* <Card text="card text" /> */}
+    </div>
+  );
+}
---
+++ first-next-app/src/pages/tailwindcss.js
@@ -0,0 +1,7 @@
+export const Tailwind = () => {
+  return (
+    <h1 className="text-3xl font-bold underline text-green-500 p-8 border-4">
+      Hello world!
+    </h1>
+  );
+};
